01ee5fb87bf8765b1b7238f86f78263454e8e6b2
Chandan V S <158762910+chanduakshu@users.noreply.github.com>
2026-01-14 13:28:27 +0530

    Implement Insertion Sort algorithm in Java

---
+++ SearchingSorting/InsertionSort.java
@@ -0,0 +1,20 @@
+// Problem: Insertion Sort
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
