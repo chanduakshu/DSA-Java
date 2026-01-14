af6a71207fda7463c1a300eab44dd46362965eac
Chandan V S <158762910+chanduakshu@users.noreply.github.com>
2026-01-14 13:22:56 +0530

    Create QueueUsingStack.java

---
+++ Queue/QueueUsingStack.java
@@ -0,0 +1,24 @@
+// Problem: Implement Queue using Stack
+// Approach: Two stacks
+// Time Complexity: Amortized O(1)
+// Space Complexity: O(n)
+
+import java.util.Stack;
+
+public class QueueUsingStack {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public void enqueue(int x) {
+        s1.push(x);
+    }
+
+    public int dequeue() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.isEmpty() ? -1 : s2.pop();
+    }
+}
