5dafe5b49253a1c44c4f44768210388b64836f71
Chandan V S <158762910+chanduakshu@users.noreply.github.com>
2026-01-14 13:28:01 +0530

    Create SelectionSort.java

---
+++ SearchingSorting/SelectionSort.java
@@ -0,0 +1,22 @@
+// Problem: Selection Sort
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
