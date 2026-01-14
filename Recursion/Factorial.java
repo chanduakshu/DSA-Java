b843c0384861e1fa0e934d0ed2e820f57f466fbf
Chandan V S <158762910+chanduakshu@users.noreply.github.com>
2026-01-14 13:24:44 +0530

    Create Factorial.java

---
+++ Recursion/Factorial.java
@@ -0,0 +1,13 @@
+ // Problem: Find factorial of a number
+// Approach: Recursion
+// Time Complexity: O(n)
+// Space Complexity: O(n) (recursion stack)
+
+public class Factorial {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+}
